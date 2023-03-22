1b1aa6e5f0027c1648b0ce34d6a132d8bd1c0847
Osahon <32203058+0sahon@users.noreply.github.com>
2023-03-22 12:50:21 +0000

    Add files via upload

---
+++ TMAX/app.js
@@ -0,0 +1,14 @@
+const first = 20;
+const second = 50;
+
+if (first < 20)
+{
+    alert("low score");
+}
+else if(second > first && first > 10){
+    alert("the value has a high score");
+}
+else
+{
+    alert("the value is correct");
+}
